a60099010cb5480ee7a0619ea15f70d7842a0fc0
bpanguluru <64513939+bpanguluru@users.noreply.github.com>
2022-01-01 05:52:52 +0000

    Update flightday.py

--- flightday.py
+++ flightday.py
@@ -5,6 +5,7 @@ import digitalio
 from adafruit_bme280 import basic as adafruit_bme280
 import pickle
 import time
+from time import sleep
 
 camera=PiCamera()
 start_time = time.time()
@@ -28,7 +29,8 @@ try:
 
     bme280.sea_level_pressure = init_alt
     passedUpPoint = False
-    while !passedUpPoint || bme280.altitude > 50: # this may malfunction due to black powder charge but hopefully not
+
+    while (passedUpPoint==True or bme280.altitude > 50): # this may malfunction due to black powder charge
         sleep(0.02) #might have to increase this to 2
         check_alt = bme280.altitude
         if check_alt > 50: #take pictures whenever alt is greater than 50 meters
@@ -55,6 +57,3 @@ except:
         img_no = i
         sleep(0.5)
         camera.capture("/home/pi/Downloads/subscale_test_imgs/img_%s.jpg" % img_no)
-        
-        
-            
