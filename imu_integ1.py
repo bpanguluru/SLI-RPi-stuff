10d1b5f1cd9e5bd55f73862c0e4d0aa773d1a980
bpanguluru <64513939+bpanguluru@users.noreply.github.com>
2022-03-27 02:16:53 -0700

    Update imu_integ1.py

--- imu_integ1.py
+++ imu_integ1.py
@@ -1,56 +1,40 @@
 import logging
 import sys
 import time
-from Adafruit_BNO055 import BNO055
+#from Adafruit_BNO055 import BNO055
 import socket, pickle , time
 from math import sqrt
 import numpy as np
 from scipy import signal
+import adafruit_bno055
 
-# Enable verbose debug logging if -v is passed as a parameter.
-if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
-    logging.basicConfig(level=logging.DEBUG)
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bno055.BNO055_I2C(i2c)
 
-# Initialize the BNO055 and stop if something went wrong.
-if not bno.begin():
-    raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+test_acc = sensor.acceleration
+test_gyro = sensor.gyro
+test_q = sensor.quaternion
+test_euler = sensor.euler
+test_linear_acc = sensor.linear_acceleration
+grav = sensor.gravity
 
-# Print system status and self test result.
-status, self_test, error = bno.get_system_status()
-print('System status: {0}'.format(status))
-print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
-# Print out an error if system status is in error mode.
-if status == 0x01:
-    print('System error: {0}'.format(error))
-    print('See datasheet section 4.3.59 for the meaning.')
-
-# Print BNO055 software revision and other diagnostic data.
-sw, bl, accel, mag, gyro = bno.get_revision()
-print('Software version:   {0}'.format(sw))
-print('Bootloader version: {0}'.format(bl))
-print('Accelerometer ID:   0x{0:02X}'.format(accel))
-print('Magnetometer ID:    0x{0:02X}'.format(mag))
-print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
-
-print('Reading BNO055 data, press Ctrl-C to quit...')
 timeslist = []
 accels = []
+q_list = []
 sum_time = 0
 while sum_time<30:
     current = time.time()
     #sleep(0.07)
-    # Read the Euler angles for heading, roll, pitch (all in degrees).
-    heading, roll, pitch = bno.read_euler()
+    
     # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
-    sys, gyro, accel, mag = bno.get_calibration_status()
    
     final = time.time()-current
     timeslist.append(final)
-    accels.append(accel)
-    
+    accels.append(sensor.acceleration)
+    quat = sensor.quaternion
+    q_list.append(quat)
     sum_time += final
-    print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
-          heading, roll, pitch, sys, gyro, accel, mag))
+    
 #put the read, save to file, and append to list into a try: except: pass in the main code
 #also add a try: except: pass to the receiver code to prevent byte error    
 
