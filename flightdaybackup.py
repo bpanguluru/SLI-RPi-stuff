82dbcebf6d6abe23b2835b1f4b88c5dde4b2e3c4
bpanguluru <64513939+bpanguluru@users.noreply.github.com>
2022-03-26 23:23:56 -0700

    Update flightdaybackup.py

--- flightdaybackup.py
+++ flightdaybackup.py
@@ -24,7 +24,39 @@ GPIO.output(17, GPIO.LOW)
 sleep(0.25)
 
 try:
-    
+    import logging
+    import sys
+    import time
+    from Adafruit_BNO055 import BNO055
+
+    # Enable verbose debug logging if -v is passed as a parameter.
+    if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
+        logging.basicConfig(level=logging.DEBUG)
+
+    # Initialize the BNO055 and stop if something went wrong.
+    if not bno.begin():
+        raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+
+    # Print system status and self test result.
+    status, self_test, error = bno.get_system_status()
+    print('System status: {0}'.format(status))
+    print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
+    # Print out an error if system status is in error mode.
+    if status == 0x01:
+        print('System error: {0}'.format(error))
+        print('See datasheet section 4.3.59 for the meaning.')
+
+    # Print BNO055 software revision and other diagnostic data.
+    sw, bl, accel, mag, gyro = bno.get_revision()
+    print('Software version:   {0}'.format(sw))
+    print('Bootloader version: {0}'.format(bl))
+    print('Accelerometer ID:   0x{0:02X}'.format(accel))
+    print('Magnetometer ID:    0x{0:02X}'.format(mag))
+    print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
+
+    print('Reading BNO055 data, press Ctrl-C to quit...')
+except:
+    pass    
 
 x_incr =(117.808914-117.809808)
 y_incr =(35.346394-35.347125)
